d55d398e83f8cabf7f025f3fd84fc73c315d27ab
media-admin <markus.tritremmel@media-lab.at>
2022-08-22 21:00:50 +0200

    Fixed slider dots

--- wp-content/themes/fliesenweissenboeck/assets/scripts/button-back-to-top.js
+++ wp-content/themes/fliesenweissenboeck/assets/scripts/button-back-to-top.js
@@ -7,7 +7,6 @@ jQuery(document).ready(function(){
 	// Der Button wird mit JavaScript erzeugt und vor dem Ende des body eingebunden.
 	var back_to_top_button = ['<a href="#top" class="back-to-top"><img src="https://fliesen-weissenboeck.dev/wp-content/themes/fliesenweissenboeck/img/topbutton.svg"></a>'].join("");
 	jQuery("body").append(back_to_top_button)
-https://www.janecka.at/wp-content/themes/janecka/assets/img/icons/button-back-to-top.svg
 	// Der Button wird ausgeblendet
 	jQuery(".back-to-top").hide();
 
